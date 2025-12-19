57e51fb8eccdf90a996bc95ea7573924a5cfba10
giulio <giulio@Debian-UFANGIG.station>
2025-12-19 23:52:11 +0100

    commit all first version

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# telegram-log-monitor
+# Telegram Log Monitor Bot
+
+This is a Python event-driven script that monitors caddy JSON logs in real time and aletrs user via Telegram Bot APIs.
+Every time new lines are added to the log file, it parse the logs retrieving critical data, such as IP address, URIs accessed and return status code. It then enriches IP details with geographical information (city and region) using ipinfo.io APIs.
+
+## Main functionalities
+- The script uses `watchdog` module to detect file changes (event-driven) to avoid poolling and improve performance;
+- Every time a new access is detected only new lines are parsed, using `tell()` and `seek()` functions, for a more efficient reading;
+- To reduce notifcation quantity and noise, the script filter only for specific URIs, considered more critical;
+- For a new IP, geographical information are retrieved using ipinfo.io APIs, while those already checked are stored in a cache, implemented with a dictionary;
+- At the end of the loop, telegram APIs are used to alert the user with a message containg all the recent accesses;
+
+## Requirements
+- python 3.10+
+
+The python modules to be installed are in the `requirements.txt` file and can be installed with:
+```
+pip3 install -r requirements.txt`
+```
+
+It is also possibile that you'll need to create a virtual environment in order to install the requirements. This can be done with:
+```
+python3 -m venv .venv
+```
+
+And then:
+```
+source /path/to/.venv/bin/activate
+```
+Before installing the needed modules.
+
+This script is not OS-specific, but in this repository at the moment only provides linux systemd service for automation.
+
+## Install & run
+The quickest way to start this script is by copying the `telegram-monitor.service` to the systemd directory and run the service. 
+You can do it in your home directory:
+```
+cp telegram-monitor.service ~/.config/systemd/system/
+systemctl --user start telegram-monitor.service
+```
+Or, if you need root privileges: 
+```
+sudo cp telegram-monitor.service /etc/systemd/system/
+sudo systemctl start telegram-monitor.service
+```
+
+In bot cases you'll need to edit the unit file with your personal paths where you script and log file are.
---
+++ telegram-log-monitor.py
@@ -0,0 +1,189 @@
+import json
+import os
+import time
+from collections import defaultdict
+from telegram import Bot # v22.5 
+import asyncio
+import aiohttp
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# bot parameters
+TELEGRAM_BOT_TOKEN = os.getenv("MY_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+LOG_DIR = os.getenv("LOG_DIR")
+LOG_FILE = os.getenv("LOG_FILE")
+LOG_PATH = os.path.join(LOG_DIR,LOG_FILE)
+
+last_pos = 0
+ip_cache = {}
+
+watched_uris = [
+        '/',
+        '/api/config',
+        '/identity/connect/token',
+        '/api/sync'
+]
+
+class LogHandler(FileSystemEventHandler):
+    def __init__(self, loop, queue):
+        self.loop = loop
+        self.queue = queue
+
+    def on_modified(self,event):
+        if event.src_path == LOG_PATH:
+            self.loop.call_soon_threadsafe(
+                self.queue.put_nowait,
+                None
+            )
+
+# bot send message
+async def send_message(text, chat_id):
+    try:
+        async with bot:
+            await bot.send_message(text=text, chat_id=chat_id)
+    except Exception as e:
+        print(e)
+        return
+
+async def run_bot(messages, chat_id):
+    text = ' '.join(messages)
+    await send_message(text, chat_id)
+
+def new_ip_entry():
+    return {
+            "first_access": None,
+            "uris": [],
+            "status": ''
+    }
+
+def new_cache_entry():
+    return {
+        'city': '',
+        'region': ''
+    }
+
+async def get_ipinfo(ip,session):
+    global ip_cache
+
+    if ip in ip_cache:
+        entry = ip_cache[ip]
+        city = ip_cache[ip]['city']
+        region = ip_cache[ip]['region']
+        if city and region:
+            return f"{city}, {region}"
+
+    try:
+        async with session.get(f"https://ipinfo.io/{ip}/json",timeout=aiohttp.ClientTimeout(total=5)) as res:
+            res.raise_for_status()
+
+            data = await res.json()
+
+            city = data.get('city','Error retrieving city')
+            region = data.get('region','Error retrieving region')
+
+            ip_cache[ip]['city'] = city
+            ip_cache[ip]['region'] = region
+
+            return f"{city}, {region}"
+    except asyncio.TimeoutError:
+        return "Connection timed out"
+    except (json.JSONDecodeError,KeyError):
+        return f"Invalid parse format"
+    except aiohttp.ClientError as e:
+        return f"Client error: {e}"
+    except Exception as e:
+        return f"{e}"
+    
+def parse_logs(lines):
+    dic = defaultdict(new_ip_entry)
+
+    for line in lines:
+        try:
+            el = json.loads(line) 
+            uri = el['request']['uri']
+
+            if uri not in watched_uris:
+                continue
+            
+            ip = el['request']['client_ip']
+            ts = el['ts']
+            status = el['status']
+            dic[ip]['uris'].append(uri)
+            dic[ip]['status'] = status
+
+            if dic[ip]['first_access'] is None:
+                dic[ip]['first_access'] = ts 
+
+        except json.JSONDecodeError:
+            continue
+    
+    return dic
+
+async def create_message(dic, session):
+    messages = []
+    for ip,infos in dic.items():
+        ip_details = await get_ipinfo(ip,session)
+        message = f"Ip {ip} ({ip_details}) accessed at {infos['first_access']} and returned status {infos['status']}.\n" 
+        if len(infos['uris']) > 0:
+            message += f"Accessed uris: {infos['uris']}."
+        else:
+            message += "Unknown uris were accessed"
+        messages.append(message)
+    
+    message_size = sum(len(m) for m in messages)
+    
+    if message_size > 4096:
+        messages=messages[-1:]
+        messages.append('Other IPs were detected but message was too long. Only last acces is being displayed')
+
+    return messages
+
+async def handle_log(session: aiohttp.ClientSession):
+    global last_pos
+    
+    with open(LOG_PATH,'r') as f:
+        f.seek(last_pos)
+        lines = f.readlines()
+        last_pos= f.tell()
+
+    dic = parse_logs(lines)
+        
+    messages = await create_message(dic,session)
+    
+    await run_bot(messages,CHAT_ID)
+
+async def process_log(queue: asyncio.Queue):
+    async with aiohttp.ClientSession() as session:
+        while True:
+            await queue.get()
+            await handle_log(session)
+
+def init_cache():
+    global ip_cache
+    ip_cache = defaultdict(new_cache_entry)
+
+async def main():
+
+    init_cache()
+    
+    loop = asyncio.get_running_loop()
+    queue = asyncio.Queue()
+
+    event_handler = LogHandler(loop,queue)
+    observer = Observer()
+    observer.schedule(event_handler,path=LOG_DIR,recursive=False)
+    observer.start()
+
+    try:
+        await process_log(queue)
+    except Exception as e:
+        print(f"Unkown error {e}")
+        exit(2)
+    finally:
+        observer.stop()
+        observer.join()
+
+asyncio.run(main())
